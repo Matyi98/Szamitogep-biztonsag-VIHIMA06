a1f947624a6f0f6f75afde2988f276fa092d3fbc
Mark Martin Kaposvari <61698307+Markesz7@users.noreply.github.com>
2022-11-03 18:37:05 +0100

    Fix parseMeta

--- main/CaffFrei/Src/CaffParser.Native/CaffParserFacade.h
+++ main/CaffFrei/Src/CaffParser.Native/CaffParserFacade.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <comdef.h>
+#include "Caff.h"
 
 extern "C" __declspec(dllexport) CaffCredits ParseMeta(const UCHAR* raw, LONG64 size);
 extern "C" __declspec(dllexport) const UCHAR* ParsePreview(const UCHAR* raw, LONG64 size);
