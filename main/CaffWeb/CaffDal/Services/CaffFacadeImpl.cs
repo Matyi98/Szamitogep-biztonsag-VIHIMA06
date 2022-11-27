491a4e23bab733ba7e25027b1cc09df1d285d915
Active-hub <61094345+Active-hub@users.noreply.github.com>
2022-11-27 21:56:20 +0100

    Beta implementation of CaffParser..

--- main/CaffWeb/CaffDal/Services/CaffFacadeImpl.cs
+++ main/CaffWeb/CaffDal/Services/CaffFacadeImpl.cs
@@ -1,11 +1,16 @@
 ﻿using CaffDal.Domain;
 using CaffDal.Domain.Pager;
 using Microsoft.EntityFrameworkCore;
+using CliWrap;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using CaffDal.Services.Parser;
+using CaffDal.Entities;
 
 namespace CaffDal.Services
 {
@@ -78,8 +83,46 @@ namespace CaffDal.Services
             throw new NotImplementedException();
         }
 
-        public Task<UploadResponse> UploadCaff(UploadRequest request)
+        public async Task<UploadResponse> UploadCaff(UploadRequest request)
         {
+            /*
+             * Execute exe and read manifest
+             * */
+            await Cli.Wrap("native_parser.exe")
+                .WithArguments(request.CaffName)
+                .ExecuteAsync();
+            string[] lines = File.ReadLines("manifest").ToArray();
+
+            /*
+             * Create Caff
+             * */
+            Caff caff = new Caff(lines[0].Split(':')[1], request.RawCaff);
+            caff.CreatorDate = CiffDateToDateAndTime(lines[1].Split(':')[1]);
+            caff.NumberOfFrames = Convert.ToInt32(lines[2].Split(':')[1]);
+            caff.Id = new Random().Next();
+            caff.UserId = request.OwnerId;
+            //caff.User = ;
+            //caff.Creator = ;
+            List<Ciff> CiffList = StringArrayToCiffList(3, lines);
+
+            /*
+             * Add ciff's to caff
+             * */
+            foreach (Ciff ciff in CiffList)
+            {
+                Image image = new(Parser.Parser.display(ciff));
+                image.Caff = caff;
+                image.Duration = ciff.Duration;
+                image.Preview = Parser.Parser.display(ciff);
+                image.CaffId = caff.Id;
+                caff.Images.Add(image);
+            }
+            /*
+             * Todo save to db
+             * */
+
+            return new UploadResponse{ CaffId = caff.Id };
+
             // TODO:
             // 1. Call C++ exe (with RawCaff, caffName)
             // 2. Read manifest file
@@ -90,9 +133,36 @@ namespace CaffDal.Services
             // 7. Save Ciff data to the Image table.
             // 8. Save changes!
             // 9. Create and return UploadResponse.
-
-            throw new NotImplementedException();
         }
+
+        private DateTime CiffDateToDateAndTime(string ciffDate)
+        {
+            string[] uglyDateArray = ciffDate.Split(' ');
+            string[] ymd = uglyDateArray[0].Split('.');
+            string[] hs = uglyDateArray[0].Split(':');
+            return new DateTime(year: Convert.ToInt32(ymd[0]), month: Convert.ToInt32(ymd[1]), day: Convert.ToInt32(ymd[2]),
+                hour: Convert.ToInt32(hs[0]), minute: Convert.ToInt32(hs[1]), second: 0);
+        }
+
+        private List<Ciff> StringArrayToCiffList(int readFrom, String[] lines)
+        {
+            List<Ciff> CiffList = new List<Ciff>();
+            for (int i = readFrom; i < lines.Length; i += 6)
+            {
+                var ciff = new Parser.Ciff(File.ReadAllBytes(lines[i]));
+                ciff.Duration = Convert.ToInt32(lines[i + 1].Split(':')[1]);
+                ciff.Caption = lines[i + 2].Split(':')[1];
+                ciff.Tags = lines[i + 3].Split(':')[1].Split(';').ToList<string>();
+                ciff.Width = Convert.ToInt32(lines[i + 4].Split(':')[1].Split('*')[0]);
+                ciff.Height = Convert.ToInt32(lines[i + 5].Split(':')[1].Split('*')[1]);
+                CiffList.Add(ciff);
+            }
+            return CiffList;
+        }
+
+
+
+
 
         public Task WriteComment(int caffId, string comment, int uploader)
         {
---
+++ main/CaffWeb/CaffDal/Services/Parser/bitmap.cs
@@ -0,0 +1,35 @@
+﻿using CaffDal.Entities;
+using System.Drawing;
+
+
+namespace CaffDal.Services.Parser
+{
+    public static class Parser
+    { 
+    public static Tuple<byte, byte, byte> read_content(byte[] x, int o)
+    {
+        return Tuple.Create(x[o], x[o + 1], x[o + 2]);
+    }
+
+    public static object display(Ciff content, bool show)
+    {
+        var offset = 0;
+            //var img = new Bitmap Image.new("RGB", (content.Width, content.Height), "magenta");
+             ImageMagick.MagickImage = new MagickImage("alma", )
+           
+        var pixels = img.load();
+        foreach (var j in Enumerable.Range(0, img.size[1]))
+        {
+            foreach (var i in Enumerable.Range(0, img.size[0]))
+            {
+                pixels[i, j] = read_content(content.RawCiff, offset);
+                offset += 3;
+            }
+        }
+        if (show)
+        {
+            img.show();
+        }
+    }
+  }
+}
